eec511134ae65b602d54bd5f109818079a8fe429
DanieleLena <daniele.lena@yahoo.it>
2021-05-13 19:43:40 +0200

    added projects section

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 
 
+function enableScrolling() {    
+    setTimeout(()=>{
+        document.body.style.overflowY = "scroll";
+    },3300);
+}
+
 function displayHero(){
 
     const heroImages = ["hero1","hero2","hero3","hero4"];
@@ -9,6 +15,26 @@ function displayHero(){
     heroContainer.innerHTML = `<img src="images/${heroImages[randomN]}.gif" alt="hero">`;
 
 };
+
+// window.onload = enableScrolling();
 window.onload = displayHero();
+
+
+// Project video 
+
+const videos = document.querySelectorAll(".video");
+
+videos.forEach((video) => {
+
+    video.addEventListener("mouseover", ()=> {
+        
+        video.play();
+        
+    })
+    video.addEventListener("mouseout", () => {
+   video.currentTime = 0;
+   video.pause();
+    });
+})
 
 
